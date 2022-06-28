a4022dc7e0306e7925f5869865ea5a1f73506f68
TênMac <caoducmanh2411999@gmail.com>
2022-06-28 11:35:42 +0300

    - LS15_AJAX_SPRING

--- src/main/webapp/assets/js/custom.js
+++ src/main/webapp/assets/js/custom.js
@@ -1,32 +1,49 @@
 jQuery(document).ready(function ($) {
+    /*
+    * Đọc file document (các files .jsp), khi các files này đã được load trên màn hình
+    * thì Jquery mới kích hoạt code trong nó
+    *
+    * Do đó cần đặt jquery.min.js ở cuối trong các files giao diện .jsp
+    * */
 
     $('#btn-register').click(function (e) {
-        e.preventDefault()
+        /*
+        * Đặt sự kiện cho button
+        * #: id - .: class
+        * */
 
+        e.preventDefault()
+        /* function(e){e.preventDefault(): do Button nằm trong thẻ form, nên mặc định
+        * sau khi nhấn Button thì sẽ chuyền sang POST/GET, lệnh này chặn việc submit form
+        * */
+
+        // mapping
         var fullname = $('#fullname').val()
         var email = $('#email').val()
         var password = $('#password').val()
 
         $.ajax({
+            // Khi click thì chuyển sang url đích
             url: 'http://localhost:8080/DemoFilter/api/register',
             type: 'get',
             data: {
+                // Truyền tham số vào link api (url)
                 fullname: fullname,
                 email: email,
                 password: password
             }
         }).done(function (result) {
+            // Thành công
             if (result.success) {
-                //Thanh cong
                 $('#message').append('<h1>Thành công</h1>')
             } else {
-                //That bai
                 $('#message').append('<h1>Thất bại</h1>')
             }
         }).fail(function (error) {
-
+            // Lỗi
         })
-
     })
-
 })
+
+
+
--- src/main/webapp/register.jsp
+++ src/main/webapp/register.jsp
@@ -72,13 +72,17 @@
     </div>
 </div>
 
+<%--Import Jquery ở cuối trang trong file giao diện .jsp--%>
 <script src="https://cdn.jsdelivr.net/npm/jquery@2.2.4/dist/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.4/dist/umd/popper.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/jquery-match-height@0.7.2/dist/jquery.matchHeight.min.js"></script>
 <script src="assets/js/main.js"></script>
-<script src="assets/js/custom.js"></script>
 
+<%--Import custom.js để sử dụng--%>
+<script src="assets/js/custom.js"></script>
 </body>
 </html>
+
+
 
